91c296fd81b16ab6c74c36a2ad03b04df8e70037
UnderZero <46681825+UnderZer0z@users.noreply.github.com>
2021-03-08 16:01:14 +0200

    lang is done yay

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,120 @@
+
+
+const langs = {
+  EN : {
+    nav:[ 'Info', 'Background' , 'Contact', 'Send A Complaint'],
+    footer:"This website is served by the National Unit for Operational   Investigations in the IDF , in order to enable the service to  the public.",
+    page1:["The National Unit for Operational Investigations in the IDF is authorized to investigate suspected criminal offenses under international humanitarian law carried out during operational activities or ongoing security by soldiers under IDF command."],
+    page2:[
+      'On January 2017, the Investigating Military Police, invented the National Unit for Operational Investigations (so called: “YAALAM”), following two government committees (Turkel 2013, Ciechanover 2015) that dealt with international humanitarian law, inter alia with respect to suspected criminal offenses committed by IDF soldiers during operational activity or ongoing security',
+      'From the report of the Turkel Committee which recommended the establishment of the unit, section 62, MPC for operational matters:',
+      '"The committee came to conclusion that alongside the State Attorney\'s Office for Operational Matters in the military prosecution, there is room to establish a department in the MPC for operational matters.',
+      'The military investigators\' appointed to the MPC for operational matters will be trained in the rules of international humanitarian law, and in particular in the duties relating to the investigation of violations of these rules.',
+      'In order to ensure direct communication with witnesses, complainants and other relevant investigators, it is recommended that interrogators who are proficient in Arabic be among the investigators. "In order to promote the MPC\'s accessibility to the complainants, it is recommended that MPIU bases for operational matters be deployed in the areas where the events under investigation take place."',
+      'From the report of the “Ciechanover” Committee, Recommendation No. 9 - Sections 91-93, MPC for operational matters and the recommendation for implementation:',
+      '"The “Turkel” Commission found that one of the requirements arising from the principle of effectiveness and thoroughness is that an investigation be conducted professionally, therefore the committee recommended the establishment of a body for operational matters in the MPC, similar to such a body operating in the military prosecutor\'s office. The dedicated body will specialize in handling investigations into complaints of violations of the rules of international humanitarian law. The committee recommended that researchers in this body receive appropriate training, and that their sons also have proficient researchers in the Arabic language. It was also recommended that MPI bases for operational matters be deployed in the areas where the investigated events take place. "',
+      '"We recommend that the military authorities establish a dedicated unit in the IDF that will investigate operational incidents. It is proposed that this unit be established and operational within 4 months from the date of approval of the recommendations of this report in the Government " "Representatives of the Military Prosecutor\'s Office informed the staff that the army adopted the recommendation and began the initial processes required for the establishment of this unit. The competent bodies in the army even instructed on the allocation of appropriate resources. The Supreme Court of the IDF or any other suitable host will be decided by the IDF. "',
+      'In addition to the conclusions of the aforesaid committees, on January 28, 2018, Prime Minister Benjamin Netanyahu decided to expand the powers of the MPIU unit (under the responsibility of the YALM) regarding the interrogation of Border Police soldiers in shooting incidents in Judea and Samaria, while operating under IDF command. A series of discussions on the subject, a decision was made that the implementation of the decision and the transfer of responsibility will be carried out starting in October 2019.'
+    ],
+    page3:[
+      ':  Ways to contact',
+      'The “YAALAM” investigates nationwide and is located in the “Anatot camp” in Jerusalem and in the “Beit Lid camp” in Sharon.. ',
+      'The “YAALAM” is in constant contact with all the coordination and linking and evidence collection facilities and is carried out at the facilities listed below.',
+      ' 052-9411594 - Yaalam’s phone number avalible 24/7',
+      ' 03-7373693 -  FAX ',
+      'yaalam6015@gmail.coml –  mail',
+      'A complaint can be left at the Civil Administration\'s coordination and liaison facilities at the following addresses and referred to YAALAM:',
+      ' Phone number CDO 050-3086865 - Ephraim',
+      'Phone number CDO 05030803222 - Jenin',
+      'Phone number CDO 02-9704646 - Qalqilya',
+      'Phone number CDO 02-9703744 - Ramallah',
+      'Phone number CDO 02-9703159 - Nablus',
+      'Phone number CDO 02-9704649 - Tulkarm ',
+      'Phone number CDO 08-6741400 - Erez border ',
+      'Phone number CDO 02-9704650 - Eyal border ',
+      'Phone number CDO 02-9703906 - Herbron',
+      'Phone number CDO 02-9703870 - Bethlehem',
+    ]
+  },
+
+  HE : {
+    nav:[ 'כללי', 'אודות' , 'דרכים ליצרת קשר', 'הגשת תלונה'],
+    page1:["כללי : היחידה הארצית לחקירות מבצעיות במצ\"ח מוסמכת לחקור חשד לעבירה פלילית, בדין ההומניטארי הבין-לאומי שהתבצעה במהלך פעילות מבצעית או ביטחון שוטף ע\"י חיילים הנמצאים תחת פיקוד צה\"ל"],
+    page2:[
+      'אודות:',
+      'בחודש ינואר 2017 הוקמה במשטרה הצבאית החוקרת, היחידה הארצית לחקירות מבצעיות (להלן: היאל"ם) בעקבות שתי ועדות ממשלתיות (טירקל 2013, צ\'חנובר 2015) אשר עסקו בדין ההומניטארי הבין-לאומי , בין היתר ביחס לחשד בעבירות פליליות שבוצעו ע"י חיילי צה"ל במהלך פעילות מבצעית או ביטחון שוטף.',
+      'מתוך דו"ח ועדת טירקל שהמליצה על הקמת היחידה, סעיף 62, מצ"ח לעניינים מבצעיים:',
+      '"הוועדה הגיעה לכלל מסקנה שבצד הפרקליטות לעניינים מבצעיים שבתביעה הצבאית, יש מקום להקים מחלקה במצ"ח לעניינים מבצעיים. השוטרים הצבאיים שימונו למצ"ח לעניינים מבצעיים יעברו הכשרו בכללי המשפט הבינלאומי ההומניטארי, ובמיוחד בחובות הנוגעות לחקירת הפרות של כללים אלה. כדי להבטיח תקשורת בלתי אמצעית עם עדים, מתלוננים וגורמים רלוונטיים נוספים לחקירה, מומלץ כי יהיו בין החוקרים גם חוקרים הבקיאים בשפה הערבית. על מנת לקדם את הנגישות של מצ"ח למתלוננים, מומלץ כי בסיסי מצ"ח לעניינים מבצעיים יתפרסו באזורים בהם מתרחשים האירועים הנחקרים"',
+      'מתוך דו"ח ועדת צ\'חנובר, המלצה מס\' 9 – סעיפים 91-93, מצ"ח לעניינים מבצעיים והמלצת היישום:',
+      '"ועדת טירקל מצאה כי אחת הדרישות הנובעות מעיקרון האפקטיביות והיסודיות היא שחקירה תיערך במקצועיות, לפיכך המליצה הוועדה להקים גוף לעניינים מבצעיים במצ"ח, בדומה לגוף כזה הפועל בפרקליטות הצבאית. הגוף הייעודי יתמחה בטיפול בחקירות של תלונות על הפרות של כללי המשפט הבין-לאומי הומניטארי. הוועדה המליצה שהחוקרים בגוף זה יקבלו הכשרות מתאימות, ושיהיו בניהם גם חוקרים הבקיאים בשפה הערבית. עוד הומלץ כי בסיסי מצ"ח לעניינים מבצעיים יתפרסו באזורים שבהם מתרחשים האירועים הנחקרים"',
+      '"אנו ממליצים לשלטונות הצבא להקים יחידה ייעודית במצ"ח שתחקור אירועים מבצעיים. מוצע כי יחידה זו תוקם ותופעל בתוך 4 חודשים ממועד אישורן של המלצות דוח זה בממשלה" "נציגי הפרקליטות הצבאית עדכנו את הצוות כי הצבא אימץ את ההמלצה והחל בתהליכים ראשוניים שנדרשים להקמת יחידה זו. הגורמים המוסמכים בצבא אף הנחו בדבר הקצאת המשאבים המתאימים. הקמת היחידה, הכשרתם המקצועית של אנשיה, הפעלתה, סמכותיה ותפקידיה – נושאים אלה חייבים לקבל ביטוח בהוראת הפיקוד העליון של צה"ל או בכל אכנסיה מתאימה אחרת שיחליט עליה הפצ"ר."',
+      'בנוסף למסקנות הוועדות האמורות, בתאריך 28 בינואר 2018 החליט ראש הממשלה בנימין נתניהו על הרחבת הסמכויות של יחידת מצ"ח (באחריות היאל"ם) בכל הנוגע לחקירת חיילי מג"ב באירועי ירי באיו"ש, בעודם פועלים תחת פיקוד צה"ל. בתום סדרת דיונים בנושא, התקבלה החלטת שיישום ההחלטה והעברת האחריות תבוצע החל מחודש אוקטובר 2019.'
+
+    ],
+    page3:[
+      'דרכים ליצירת קשר :',
+      'היאל"ם חוקרת בפריסה ארצית ממוקמת במחנה ענתות בירושלים ובמחנה בית ליד שבשרון. היאל"ם בקשר שוטף עם כלל מתקני התיאום וקישור וגביית עדות מתבצעת במתקנים המפורטים מטה.',
+      'חמ"ל יאל"ם זמין 24/7 – 052-9411594',
+      'פקס – 03-7373693',
+      'מייל – yaalam6015@gmail.com',
+      'ניתן להשאיר תלונה במתקני התיאום והקישור של המנהל האזרחי בכתובות הבאות ולהפנות ליאל"ם-',
+      'אלפון מת"קים :',
+      'חמ"ל מת"ק אפרים – 050-3086865',
+      'חמ"ל מת"ק ג\'נין – 05030803222',
+      'חמ"ל מת"ק קלקיליה – 02-9704646',
+      'חמ"ל מת"ק רמאללה – 02-9703704',
+      'חמ"ל מת"ק שכם – 02-9703160',
+      'חמ"ל מת"ק טול כרם- 02-9704649',
+      'חמ"ל מעבר ארז – 08-6741400',
+      'חמ"ל מעבר אייל – 02-9704650',
+      'חמ"ל חברון – 02-9703905',
+      'מת"ק בית לחם – 02-9703870'
+    ],
+    footer:"אתר זה מופעל ע\"י היחידה מתוך מטרה להנגיש את השירות לציבור."
+
+  },
+
+  AR : {
+    nav:[ 'معلومات', 'خلفية' , 'اتصل', 'شروط الامتثال للشكوى'],
+    page1:["معلومات : الوحدة الوطنية للتحقيقات العملياتية في جيش الدفاع الإسرائيلي مخولة بالتحقيق في الجرائم الجنائية المشتبه بها بموجب القانون الإنساني الدولي والتي يتم تنفيذها أثناء الأنشطة العملياتية أو الأمن المستمر من قبل الجنود تحت قيادة جيش الدفاع الإسرائيلي."],
+    page2:[
+      "في كانون الثاني / يناير 2017 ، تم إنشاء وحدة التحقيق في الشرطة العسكرية ، وهي الوحدة الوطنية للتحقيقات العملياتية (المشار إليها فيما يلي بـ: “YAALAM”) ، في أعقاب لجنتين حكوميتين (توركل 2013 ، سيشانوفر 2015) تناولتا القانون الإنساني الدولي ، من بين أمور أخرى ، فيما يتعلق بالجرائم الجنائية المشتبه بها التي ارتكبها جنود جيش الدفاع الإسرائيلي خلال نشاط عملياتي أو أمني مستمر. ",
+      "من تقرير لجنة توركل التي أوصت بإنشاء الوحدة ، القسم 62 ، MPC للمسائل التشغيلية:",
+      "توصلت اللجنة إلى استنتاج عام مفاده أنه إلى جانب مكتب المدعي العام لشؤون العمليات في النيابة العسكرية ، هناك مجال لإنشاء دائرة في لجنة السياسة النقدية للأمور التشغيلية. سيتم تدريب ضباط الشرطة العسكرية المعينين في لجنة السياسة النقدية للشؤون التشغيلية على قواعد القانون الإنساني الدولي ، ولا سيما في الواجبات المتعلقة بالتحقيق في انتهاكات هذه القواعد. من أجل تعزيز إمكانية وصول لجنة السياسة النقدية إلى المشتكين ، يوصى بنشر قواعد MPCP للمسائل التشغيلية في المناطق التي تجري فيها الأحداث التي تم التحقيق فيها \".",
+      "من تقرير لجنة Ciechanover ، التوصية رقم 9 - الأقسام 91-93 ، MPC للمسائل التشغيلية والتوصية للتنفيذ: ",
+      "\"وجدت لجنة توركل أن أحد المتطلبات الناشئة عن مبدأ الفعالية والشمولية هو إجراء تحقيق بطريقة مهنية ، لذلك أوصت اللجنة بإنشاء هيئة للأمور التشغيلية في لجنة السياسة النقدية ، على غرار هذه الهيئة التي تعمل في مكتب المدعي العسكري. وستتخصص الهيئة المخصصة في التعامل مع التحقيقات في الشكاوى المتعلقة بانتهاكات قواعد القانون الدولي الإنساني. وأوصت اللجنة بأن يتلقى الباحثون في هذه الهيئة التدريب المناسب ، وأن يكون لأبنائهم باحثون بارعون في اللغة العربية. كما تمت التوصية بنشر قواعد MPI للمسائل التشغيلية في المناطق التي تجري فيها الأحداث التي تم التحقيق فيها \".",
+      '"نوصي بأن تنشئ السلطات العسكرية وحدة مخصصة في جيش الدفاع الإسرائيلي للتحقيق في الحوادث العملياتية. يقترح إنشاء هذه الوحدة وتشغيلها في غضون 4 أشهر من تاريخ الموافقة على التوصيات الواردة في هذا التقرير في الحكومة "',
+      '"أبلغ ممثلو النيابة العسكرية الموظفين أن الجيش تبنى التوصية وبدأ الإجراءات الأولية المطلوبة لإنشاء هذه الوحدة ، بل إن الجهات المختصة في الجيش أصدرت تعليمات بشأن تخصيص الموارد المناسبة. يقرر الجيش الإسرائيلي المحكمة العليا لجيش الدفاع الإسرائيلي أو أي مضيف مناسب آخر"',
+      "بالإضافة إلى استنتاجات اللجان المذكورة ، في 28 كانون الثاني / يناير 2018 ، قرر رئيس الوزراء بنيامين نتنياهو توسيع صلاحيات وحدة MPIU (تحت مسؤولية YALM) فيما يتعلق باستجواب جنود حرس الحدود في حوادث إطلاق نار في يهودا والسامرة ، أثناء العمل تحت قيادة الجيش الإسرائيلي. وسلسلة نقاشات حول هذا الموضوع ، تم اتخاذ قرار بتنفيذ القرار ونقل المسؤولية ابتداء من شهر أكتوبر 2019."
+
+    ],
+    page3:[
+      'طرق الاتصال :',
+      'يتولى YALAM التحقيقات في جميع أنحاء البلاد ويقع في مخيم عناتوت في القدس وفي مخيم بيت ليد في شارون.',
+      'إن YAALAM على اتصال دائم مع جميع مرافق التنسيق والربط وجمع الأدلة ويتم تنفيذها في المرافق المذكورة أدناه.',
+      'شامال يلام متاح 24/7 - 9411594-052 ',
+      'فاكس - 03-7373693',
+      'البريد - yaalam6015@gmail.com ',
+      'يمكن ترك الشكوى في مراكز التنسيق والارتباط التابعة للإدارة المدنية على العناوين التالية وإحالتها إلى YAALAM  - ',
+      'رقم الهاتف DCO : 050-3086865 - افرايم',
+      'رقم الهاتف DCO : 05030803222 - جنين',
+      'رقم الهاتف DCO : 02-9704646 - قلقيلية',
+      'رقم الهاتف DCO : 02-9703744 - رام الله',
+      'رقم الهاتف DCO : 02-9703159 - نابلس',
+      'رقم الهاتف DCO : 02-9704649 - طولكرم',
+      'رقم الهاتف DCO : 08-6741400 - معبر إيريز',
+      'رقم الهاتف DCO : 02-9704650 - ممر إيال',
+      'رقم الهاتف DCO : 02-9703906 - الخليل',
+      'رقم الهاتف DCO : 02-9703870 - بيت لحم'
+    ],
+    footer:"يتم تشغيل هذا الموقع من قبل الوحدة لغرض جعل الخدمة في متناول الجمهور."
+
+  }
+} 
+
+
+
+
 var x = document.getElementById("menu"); //gets the navbar menu
 var menu = false ; //not wroking
 
@@ -33,68 +150,54 @@ var bars = document.querySelectorAll('#bar')
 var footer = document.querySelector('#footer')
 var sub = document.querySelector('#sub')
 var subtype = sub.attributes[1].value
+var body = document.getElementsByTagName('body')
 
 
 var lang = document.getElementById('lang');
 lang.addEventListener('change', (event) => {
   console.log(event.target.value)
-
   let set = event.target.value;
-
-      // Navbar replasment
-      bars.forEach((element, i) => {
-        element.textContent = langs[set].nav[i]
-      });
-    
-    
-      //updating the main text on the page
-      sub.textContent = '';
-      langs[set][subtype].forEach((element) =>{
-        let p = document.createElement('p').innerText = element;
-        sub.append(p)
-      });
-    
-    
-      // updating the footer
-      footer.textContent = langs[set].footer
-
+  document.cookie = set
+  changeLang(set) 
 });
 
 
 
 
-const langs = {
-  EN : {
-    nav:[ 'Info', 'Background' , 'Contact', 'Send A Complaint'],
-    footer:"This website is served by the National Unit for Operational   Investigations in the IDF , in order to enable the service to  the public.",
-    page1:["The National Unit for Operational Investigations in the IDF is authorized to investigate suspected criminal offenses under international humanitarian law carried out during operational activities or ongoing security by soldiers under IDF command."],
-    page2:[
-      'On January 2017, the Investigating Military Police, invented the National Unit for Operational Investigations (so called: “YAALAM”), following two government committees (Turkel 2013, Ciechanover 2015) that dealt with international humanitarian law, inter alia with respect to suspected criminal offenses committed by IDF soldiers during operational activity or ongoing security',
-      'From the report of the Turkel Committee which recommended the establishment of the unit, section 62, MPC for operational matters:',
-      '"The committee came to conclusion that alongside the State Attorney\'s Office for Operational Matters in the military prosecution, there is room to establish a department in the MPC for operational matters.',
-      'The military investigators\' appointed to the MPC for operational matters will be trained in the rules of international humanitarian law, and in particular in the duties relating to the investigation of violations of these rules.',
-      'In order to ensure direct communication with witnesses, complainants and other relevant investigators, it is recommended that interrogators who are proficient in Arabic be among the investigators. "In order to promote the MPC\'s accessibility to the complainants, it is recommended that MPIU bases for operational matters be deployed in the areas where the events under investigation take place."',
-      'From the report of the “Ciechanover” Committee, Recommendation No. 9 - Sections 91-93, MPC for operational matters and the recommendation for implementation:',
-      '"The “Turkel” Commission found that one of the requirements arising from the principle of effectiveness and thoroughness is that an investigation be conducted professionally, therefore the committee recommended the establishment of a body for operational matters in the MPC, similar to such a body operating in the military prosecutor\'s office. The dedicated body will specialize in handling investigations into complaints of violations of the rules of international humanitarian law. The committee recommended that researchers in this body receive appropriate training, and that their sons also have proficient researchers in the Arabic language. It was also recommended that MPI bases for operational matters be deployed in the areas where the investigated events take place. "',
-      '"We recommend that the military authorities establish a dedicated unit in the IDF that will investigate operational incidents. It is proposed that this unit be established and operational within 4 months from the date of approval of the recommendations of this report in the Government " "Representatives of the Military Prosecutor\'s Office informed the staff that the army adopted the recommendation and began the initial processes required for the establishment of this unit. The competent bodies in the army even instructed on the allocation of appropriate resources. The Supreme Court of the IDF or any other suitable host will be decided by the IDF. "',
-      'In addition to the conclusions of the aforesaid committees, on January 28, 2018, Prime Minister Benjamin Netanyahu decided to expand the powers of the MPIU unit (under the responsibility of the YALM) regarding the interrogation of Border Police soldiers in shooting incidents in Judea and Samaria, while operating under IDF command. A series of discussions on the subject, a decision was made that the implementation of the decision and the transfer of responsibility will be carried out starting in October 2019.'
-    ],
-    page3:[
-      
-    ]
-  },
 
-  HE : {
-    nav:[ 'כללי', 'אודות' , 'דרכים ליצרת קשר', 'הגשת תלונה'],
-    page1:["כללי : היחידה הארצית לחקירות מבצעיות במצ\"ח מוסמכת לחקור חשד לעבירה פלילית, בדין ההומניטארי הבין-לאומי שהתבצעה במהלך פעילות מבצעית או ביטחון שוטף ע\"י חיילים הנמצאים תחת פיקוד צה\"ל"],
-    footer:"אתר זה מופעל ע\"י היחידה מתוך מטרה להנגיש את השירות לציבור."
 
-  },
+function changeLang(set) {
 
-  AR : {
-    nav:[ 'معلومات', 'خلفية' , 'اتصل', 'شروط الامتثال للشكوى'],
-    page1:["معلومات : الوحدة الوطنية للتحقيقات العملياتية في جيش الدفاع الإسرائيلي مخولة بالتحقيق في الجرائم الجنائية المشتبه بها بموجب القانون الإنساني الدولي والتي يتم تنفيذها أثناء الأنشطة العملياتية أو الأمن المستمر من قبل الجنود تحت قيادة جيش الدفاع الإسرائيلي."],
-    footer:"يتم تشغيل هذا الموقع من قبل الوحدة لغرض جعل الخدمة في متناول الجمهور."
 
-  }
-} 
+  // Navbar replasment
+  bars.forEach((element, i) => {
+    element.textContent = langs[set].nav[i]
+  });
+
+
+  //updating the main text on the page
+  sub.textContent = '';
+  langs[set][subtype].forEach((element) =>{
+    var para = document.createElement("p");
+    var node = document.createTextNode(element);
+    para.appendChild(node);
+    sub.append(para)
+
+    // if(set == 'EN'){
+    //   document.body.style.direction = "ltr";
+    // }
+    // else{
+    //   document.body.style.direction = "rtl";
+    // }
+
+  });
+
+
+  // updating the footer
+  footer.textContent = langs[set].footer
+}
+
+
+
+changeLang(document.cookie)
+lang.value = document.cookie
